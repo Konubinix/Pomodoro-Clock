05fca6d5757166028787d7b55d745b34040dbf7b
durantjay <durantjay1@gmail.com>
2015-12-08 10:38:54 -0800

    Add final css changes and edits to the sass directories

--- test-js/resettimer.js
+++ test-js/resettimer.js
@@ -1,15 +1,17 @@
-
-var resetButton = $("#resetButton");
-
-resetButton.on("click",resetTimer);
-
+//resetTimer breaks the current setInterval and resets the Pomodoro Timer
 function resetTimer(){
+	//clear setInterval
 	clearInterval(loopingTimer);
+	//change timer to match the work duration string
 	$("#timer-clock").html(function(index,oldhtml){
 		return $("#work-time").html();
 	});
+	//reenable the add, subtract, and start timer events to allow for the user to customize the pomodoro sequence
+	$(".timer-filler").css("top", "100%");
 	subtractTime.on("click",changeSessionValues);
 	addTime.on("click",changeSessionValues);
+	engageButton.off();
 	engageButton.on("click",startCountDown);
+	engageButton.html("Start");
 }
 
